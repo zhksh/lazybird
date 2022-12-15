6e20021df628de3a4dc04d87544bdf17bcb746e4
Melvin <melvintjiok@gmail.com>
2022-12-15 07:56:19 +0100

    clean up mocking of data

--- app/src/main/java/com/example/blog_e/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/blog_e/ui/home/HomeFragment.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.ToggleButton
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -26,34 +26,50 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+        val homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-
-        val postViewList: ArrayList<PostsViewModel> = arrayListOf()
-
-        val post = PostsViewModel(
-            profilePicture = 1,
-            username = "Max Mustermann"
-        )
+        val toggleButton: ToggleButton = binding.toggleButton
 
         val recyclerView: RecyclerView = binding.postsListRecyclerView
         recyclerView.setHasFixedSize(true)
         recyclerView.layoutManager = LinearLayoutManager(root.context)
 
-        for (i in 1..20) {
-            postViewList.add(post)
-        }
+        // Setup dummy data list; default should be follower list
+        val postViewList: ArrayList<PostsViewModel> = arrayListOf()
+
+        postViewList.addAll(
+            generatePosts(
+                20,
+                "Hello everyone!\nThis is a post from your followers"
+            )
+        )
 
         recyclerView.adapter = PostsViewAdapter(postViewList)
 
+        toggleButton.setOnCheckedChangeListener { _, showGlobal ->
+            if (showGlobal) {
+                postViewList.clear()
+                postViewList.addAll(
+                    generatePosts(
+                        20,
+                        "Hello worlds!\nThis is a post from someone globally\nHurray!\n\n#global"
+                    )
+                )
+                recyclerView.adapter = PostsViewAdapter(postViewList)
+            } else {
+                postViewList.clear()
+                postViewList.addAll(
+                    generatePosts(
+                        20,
+                        "Hello everyone!\nThis is a post from your followers"
+                    )
+                )
+                recyclerView.adapter = PostsViewAdapter(postViewList)
+            }
+        }
 
         return root
     }
@@ -62,4 +78,19 @@ class HomeFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+}
+
+// TODO: replace this function with service calls for fetching the posts
+fun generatePosts(number: Int, content: String): List<PostsViewModel> {
+    val posts: ArrayList<PostsViewModel> = arrayListOf()
+    for (i in 0..number) {
+        posts.add(
+            PostsViewModel(
+                profilePicture = 1,
+                username = "Max Mustermann",
+                content = content
+            )
+        )
+    }
+    return posts
 }
