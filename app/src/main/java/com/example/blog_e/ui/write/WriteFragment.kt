eea89782820731d5f40f5bf2de866dabfde0dd62
Melvin <melvintjiok@gmail.com>
2022-12-18 07:59:11 +0100

    initial design of write/editing page

--- app/src/main/java/com/example/blog_e/ui/write/WriteFragment.kt
+++ app/src/main/java/com/example/blog_e/ui/write/WriteFragment.kt
@@ -1,10 +1,11 @@
 package com.example.blog_e.ui.write
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.blog_e.databinding.FragmentWriteBinding
@@ -28,12 +29,40 @@ class WriteFragment : Fragment() {
         _binding = FragmentWriteBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textWrite
-        writeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        val postBtn = binding.postButton
+        postBtn.isEnabled = false
+
+        val aiSwitch = binding.aiSwitchButton
+        val postInput = binding.postInput
+        val emotionButtonsGroup = binding.emotionButtonsGroup
+
+        // add text watcher for empty input
+        postInput.addTextChangedListener(
+            object : TextWatcher {
+                override fun afterTextChanged(s: Editable?) {
+                }
+
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    postBtn.isEnabled = s.toString().isNotBlank()
+                }
+            }
+
+        )
+
+        aiSwitch.setOnCheckedChangeListener { _, isChecked -> println("Switch state=$isChecked") }
+
+
         return root
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
