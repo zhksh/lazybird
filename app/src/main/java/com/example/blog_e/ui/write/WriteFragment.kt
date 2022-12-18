5332eddfb3f4acd704ddc1af7bbe2ca4e7db5cb7
Melvin <melvintjiok@gmail.com>
2022-12-18 17:49:54 +0100

    add more features to write fragment

--- app/src/main/java/com/example/blog_e/ui/write/WriteFragment.kt
+++ app/src/main/java/com/example/blog_e/ui/write/WriteFragment.kt
@@ -2,12 +2,13 @@ package com.example.blog_e.ui.write
 
 import android.os.Bundle
 import android.text.Editable
+import android.text.SpannableStringBuilder
 import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
 import com.example.blog_e.databinding.FragmentWriteBinding
 
 class WriteFragment : Fragment() {
@@ -18,23 +19,28 @@ class WriteFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    private val writeViewModel: WriteViewModel by viewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val writeViewModel =
-            ViewModelProvider(this).get(WriteViewModel::class.java)
 
         _binding = FragmentWriteBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val postBtn = binding.postButton
-        postBtn.isEnabled = false
-
+        val generatePromptBtn = binding.generatePostFromNothingButton
+        val generateEmptyBtn = binding.generatePostFromPromptButton
         val aiSwitch = binding.aiSwitchButton
         val postInput = binding.postInput
-        val emotionButtonsGroup = binding.emotionButtonsGroup
+
+
+        writeViewModel.postText.observe(viewLifecycleOwner) { post ->
+            postInput.text = SpannableStringBuilder(post)
+        }
+
 
         // add text watcher for empty input
         postInput.addTextChangedListener(
@@ -51,14 +57,43 @@ class WriteFragment : Fragment() {
                 }
 
                 override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                    postBtn.isEnabled = s.toString().isNotBlank()
+                    if (s.toString().isNotBlank()) {
+                        postBtn.isEnabled = true
+
+                        generatePromptBtn.visibility = View.GONE
+                        generateEmptyBtn.isEnabled = true
+
+                    } else {
+
+                        postBtn.isEnabled = false
+                        generateEmptyBtn.isEnabled = false
+                        generatePromptBtn.visibility = View.VISIBLE
+                    }
                 }
             }
-
         )
 
-        aiSwitch.setOnCheckedChangeListener { _, isChecked -> println("Switch state=$isChecked") }
+        aiSwitch.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                binding.aiOptions.visibility = View.VISIBLE
+                generateEmptyBtn.visibility = View.VISIBLE
+                generatePromptBtn.visibility = View.VISIBLE
+                binding.aiOptions.visibility = View.VISIBLE
+            } else {
+                binding.aiOptions.visibility = View.GONE
+                generateEmptyBtn.visibility = View.GONE
+                generatePromptBtn.visibility = View.GONE
+                binding.aiOptions.visibility = View.GONE
+            }
+        }
 
+        postBtn.setOnClickListener {
+            writeViewModel.savePost()
+        }
+
+        generateEmptyBtn.setOnClickListener {
+            postInput.text = SpannableStringBuilder(postInput.text.toString() + "\nGenerated text")
+        }
 
         return root
     }
