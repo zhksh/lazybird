109d50cdb41f576e854fba8b577b75d9a4489be6
Melvin Again <melvintjiok@gmail.com>
2022-12-25 12:50:13 +0100

    define further retrofit APIs

--- app/src/main/java/com/example/blog_e/data/model/APIModel.kt
+++ app/src/main/java/com/example/blog_e/data/model/APIModel.kt
@@ -16,3 +16,46 @@ data class UserAPIModel(
     val iconId: String,
     val username: String
 )
+
+data class PostRequest(
+    val content: String,
+    val mood: String,
+    val autogenerateAnswers: Boolean,
+    val shouldAutoComplete: Boolean,
+    val temperature: Int
+)
+
+data class PostsRequest(
+    val isUserFeed: Boolean,
+    val pageSize: Int,
+    val pageToken: String,
+    val usernames: List<String>
+)
+
+data class PostsResult(
+    val nextPageToken: String,
+    val posts: List<PostAPIModel>
+)
+
+data class NewUserAPIModel(
+    val displayName: String,
+    val password: String,
+    val iconId: String,
+    val username: String
+)
+
+data class Authorization(
+    val accessToken: String,
+    val tokenType: String
+)
+
+data class LoginPayload(
+    val password: String,
+    val username: String
+)
+
+data class FollowResult(
+    val code: Int,
+    val message: String
+)
+
