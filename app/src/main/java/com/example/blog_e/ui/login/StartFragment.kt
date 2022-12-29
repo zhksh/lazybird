ea8259190fbbc5249cc9931729db0e1d45db5013
Melvin Again <melvintjiok@gmail.com>
2022-12-29 09:08:34 +0100

    add signup + start fragment and add to navigation graph

---
+++ app/src/main/java/com/example/blog_e/ui/login/StartFragment.kt
@@ -0,0 +1,30 @@
+package com.example.blog_e.ui.login
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.blog_e.databinding.FragmentStartBinding
+
+
+class StartFragment : Fragment() {
+
+    private var _binding: FragmentStartBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val sharedViewModel = ViewModelProvider(this)[LoginViewModel::class.java]
+        _binding = FragmentStartBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        return root
+    }
+}
