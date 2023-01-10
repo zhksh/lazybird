428e3c264bd5b994f21da3ef6528c416b2eb6d66
ChrisOnManjaro <20260809+schmizzel@users.noreply.github.com>
2023-01-10 13:14:03 +0100

    refactoring signup & loading spinner

--- app/src/main/java/com/example/blog_e/ui/signUp/SignUpFragment.kt
+++ app/src/main/java/com/example/blog_e/ui/signUp/SignUpFragment.kt
@@ -25,69 +25,17 @@ class SignUpFragment : Fragment() {
 
     private var _binding: FragmentSignUpBinding? = null
     private val binding get() = _binding!!
-    private val signUpViewModel: SignUpViewModel by viewModels()
+    private val viewModel: SignUpViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentSignUpBinding.inflate(inflater, container, false)
-        binding.viewModel = signUpViewModel
-
         val root: View = binding.root
 
-        setupFragmentBinding()
+        binding.viewModel = viewModel
 
         return root
-    }
-
-    private fun setupFragmentBinding() {
-        binding.etUserName.addTextChangedListener(object : TextWatcher {
-            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-                signUpViewModel.validateUsername(s.toString())
-            }
-
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
-
-            override fun afterTextChanged(p0: Editable?) {}
-        })
-
-        binding.etPassword.addTextChangedListener(object : TextWatcher {
-            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-                signUpViewModel.validatePassword(s.toString())
-            }
-
-            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
-
-            override fun afterTextChanged(s: Editable) {}
-        })
-        binding.btnSignUp.setOnClickListener { signUp() }
-    }
-
-    private fun signUp() {
-
-        val newUser = User(
-            username = binding.etUserName.text.toString(),
-            password = binding.btnSignUp.text.toString(),
-            profilePicture = ProfilePicture.PICTURE_01
-        )
-
-        //TODO: create a loading spinner
-
-        lifecycleScope.launch {
-            val isValidUser = signUpViewModel.signUp(newUser)
-            if (isValidUser) {
-                findNavController().navigate(R.id.action_sign_up_fragment_to_navigation_home)
-                Snackbar.make(
-                    binding.root,
-                    "Welcome, ${newUser.username}!",
-                    Snackbar.LENGTH_LONG
-                ).show()
-
-            } else {
-                Snackbar.make(binding.root, "User already exists", Toast.LENGTH_SHORT)
-                    .show()
-            }
-        }
     }
 }
--- app/src/main/java/com/example/blog_e/ui/signUp/SignUpViewModel.kt
+++ app/src/main/java/com/example/blog_e/ui/signUp/SignUpViewModel.kt
@@ -1,14 +1,24 @@
 package com.example.blog_e.ui.signUp
 
+import android.view.View
+import android.widget.Toast
+import androidx.databinding.ObservableBoolean
 import androidx.databinding.ObservableField
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.viewModelScope
+import androidx.navigation.fragment.findNavController
+import com.example.blog_e.R
+import com.example.blog_e.data.model.ProfilePicture
 import com.example.blog_e.data.model.User
 import com.example.blog_e.data.repository.ApiError
 import com.example.blog_e.data.repository.ApiException
 import com.example.blog_e.data.repository.ApiSuccess
 import com.example.blog_e.data.repository.UserRepo
 import com.example.blog_e.utils.SessionManager
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import java.util.Observable
 import javax.inject.Inject
 
@@ -18,11 +28,70 @@ class SignUpViewModel @Inject constructor(
     private val userRepo: UserRepo,
     private val sessionManager: SessionManager
 ) : ViewModel() {
-
     private val usernameRegex = """^[A-Za-z0-9]*$""".toRegex()
+    private var username = ""
+    private var password = ""
 
-    var usernameError = ObservableField<String>()
-    var passwordError = ObservableField<String>()
+    val usernameError = ObservableField<String>()
+    val passwordError = ObservableField<String>()
+    val isLoading = ObservableBoolean(false)
+
+    fun onClickSignUp(view: View) {
+        val newUser = User(
+            username = this.username,
+            password = this.password,
+            profilePicture = ProfilePicture.PICTURE_01  // TODO: Replace with selected image
+        )
+
+        viewModelScope.launch {
+            isLoading.set(true)
+
+            val success = signUp(newUser)
+            if (success) {
+                println("logged in success")
+            } else {
+                println("logged in failure")
+            }
+
+            isLoading.set(false)
+        }
+    }
+
+    fun updatedUsername(s: CharSequence, start: Int, before: Int, count: Int) {
+        this.username = s.toString()
+        val error = this.validateUsername(this.username)
+        this.usernameError.set(error)
+    }
+
+    fun updatedPassword(s: CharSequence, start: Int, before: Int, count: Int) {
+        this.password = s.toString()
+        val error = this.validatePassword(this.password)
+        this.passwordError.set(error)
+    }
+
+    private fun validateUsername(name: String): String {
+        if (name.length < 3) {
+            return "username too short"
+        }
+
+        if (name.length > 15) {
+            return "username too long"
+        }
+
+        if (!this.usernameRegex.matches(name)) {
+            return "only letters and numbers allowed in username"
+        }
+
+        return ""
+    }
+
+    private fun validatePassword(password: String): String {
+        if (password.length < 6) {
+            return "password too short"
+        }
+
+        return  ""
+    }
 
     suspend fun signUp(user: User): Boolean =
         when (val authorizationResult = userRepo.signUp(user)) {
@@ -31,36 +100,8 @@ class SignUpViewModel @Inject constructor(
                 true
             }
             is ApiError -> {
-                false
+                false       // TODO: Display api error
             }
             is ApiException -> throw authorizationResult.e
         }
-
-    fun validateUsername(name: String) {
-        if (name.length < 3) {
-            this.usernameError.set("username too short")
-            return
-        }
-
-        if (name.length > 15) {
-            this.usernameError.set("username too long")
-            return
-        }
-
-        if (!this.usernameRegex.matches(name)) {
-            this.usernameError.set("only letters and numbers allowed in username")
-            return
-        }
-
-        this.usernameError.set("")
-    }
-
-    fun validatePassword(password: String) {
-        if (password.length < 6) {
-            this.passwordError.set("password too short")
-            return
-        }
-
-        this.passwordError.set("")
-    }
 }
