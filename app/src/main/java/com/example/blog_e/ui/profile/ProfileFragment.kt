26b54d446b4795921659d8af2be14c2ef702b894
ChrisOnManjaro <20260809+schmizzel@users.noreply.github.com>
2023-01-12 21:50:17 +0100

    merged profile fragment

--- app/src/main/java/com/example/blog_e/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/blog_e/ui/profile/ProfileFragment.kt
@@ -7,17 +7,20 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.blog_e.R
 import com.example.blog_e.adapters.PostsViewAdapter
+import com.example.blog_e.data.model.PostAPIModel
 import com.example.blog_e.data.model.User
 import com.example.blog_e.databinding.FragmentProfileBinding
 import com.example.blog_e.models.PostsViewModel
 // import com.example.blog_e.ui.home.generatePosts
 import com.example.blog_e.utils.SessionManager
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -28,6 +31,7 @@ class ProfileFragment: Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private lateinit var recyclerView: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -52,32 +56,16 @@ class ProfileFragment: Fragment() {
             }
         })
 
-        /*
-        val textView: TextView = binding.textProfile
-        profileViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        */
-
-        val recyclerView: RecyclerView = binding.postsListRecyclerView
+        recyclerView = binding.postsListRecyclerView
         recyclerView.isNestedScrollingEnabled = false
         recyclerView.layoutManager = LinearLayoutManager(root.context)
 
-        // Setup dummy data list; default should be follower list
-        val postViewList: ArrayList<PostsViewModel> = arrayListOf()
-
-        /*
-        postViewList.addAll(
-            generatePosts(
-                20,
-                "Hello everyone!\nThis is a post from me \uD83D\uDC24\n\nMerry Christmas \uD83C\uDF85",
-                R.drawable.baby_yoda_1
-            )
-        )
-
-        recyclerView.adapter = PostsViewAdapter(postViewList)
-    */
-
+        lifecycleScope.launch {
+            // TODO: We might want to consider moving the data fetching to view model and only subscribe here through LiveData or any other observer
+            // TODO: See https://uni2work.ifi.lmu.de/course/W22/IfI/MSP/file/Vorlesung%205:%20Modern%20Android%20Architecture/show
+            val postViewList: List<PostAPIModel> = profileViewModel.fetchBlogs()
+            recyclerView.adapter = PostsViewAdapter(postViewList)
+        }
 
         return root
     }
--- app/src/main/java/com/example/blog_e/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/com/example/blog_e/ui/profile/ProfileViewModel.kt
@@ -1,18 +1,23 @@
 package com.example.blog_e.ui.profile
 
+import android.util.Log
 import android.view.View
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.blog_e.data.model.PostAPIModel
 import com.example.blog_e.data.model.ProfilePicture
 import com.example.blog_e.data.model.User
+import com.example.blog_e.data.repository.*
 import com.example.blog_e.utils.SessionManager
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class ProfileViewModel @Inject constructor(
-    private val sessionManager: SessionManager
+    private val sessionManager: SessionManager,
+    private val blogRepo: BlogRepo,
+    private val userRepo: UserRepo
 ) : ViewModel() {
 
     private val user: MutableLiveData<User?> = MutableLiveData()
@@ -36,4 +41,31 @@ class ProfileViewModel @Inject constructor(
             user.value = null
         }
     }
+
+    suspend fun fetchBlogs(
+        pageSize: Int = 20,
+        pageToken: String? = null
+    ): List<PostAPIModel> {
+        val postsResult = blogRepo.getPosts(
+            usernames = listOf("me"),
+            pageSize = pageSize,
+            isUserFeed = false,
+            pageToken = pageToken
+        )
+
+        return when (postsResult) {
+            is ApiSuccess -> {
+                // TODO: set next page token / store it
+                postsResult.data.nextPageToken
+
+                postsResult.data.posts
+            }
+            is ApiError -> {
+                Log.e(this.toString(), "Wähhhhhh") // do something
+                emptyList()
+            }
+            is ApiException -> throw postsResult.e
+        }
+    }
+
 }
