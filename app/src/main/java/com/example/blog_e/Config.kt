108f45712be24755200cd0dc05f47149845ccea5
Pascal Guldener <p.guldener@campus.lmu.de>
2023-01-17 15:24:29 +0100

    Resolve "implement post"

--- app/src/main/java/com/example/blog_e/Config.kt
+++ app/src/main/java/com/example/blog_e/Config.kt
@@ -2,8 +2,8 @@ package com.example.blog_e
 
 class Config {
     companion object {
-        const val apiAddress = "https://mvsp-api.ncmg.eu"
-//        const val apiAddress = "http://10.0.2.2:6969"
+//        const val apiAddress = "https://mvsp-api.ncmg.eu"
+        const val apiAddress = "http://10.0.2.2:6969"
         const val clientTimout = 60 //s
         const val tagPref = "mvsp"
 
--- app/src/main/java/com/example/blog_e/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/blog_e/ui/home/HomeFragment.kt
@@ -1,6 +1,7 @@
 package com.example.blog_e.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,11 +9,14 @@ import android.widget.ToggleButton
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.blog_e.Config
 import com.example.blog_e.adapters.PostsViewAdapter
 import com.example.blog_e.data.model.PostAPIModel
 import com.example.blog_e.databinding.FragmentHomeBinding
+import com.example.blog_e.utils.Utils
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
@@ -20,6 +24,7 @@ import kotlinx.coroutines.launch
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
+    private val TAG = Config.tag(this.toString())
     private val binding get() = _binding!!
     private val homeViewModel: HomeViewModel by viewModels()
     private lateinit var recyclerView: RecyclerView
@@ -29,6 +34,7 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        Log.d(TAG, Utils.formatBackstack(findNavController()))
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
@@ -42,7 +48,6 @@ class HomeFragment : Fragment() {
         // fetch blogs from user feed
         lifecycleScope.launch {
             val postViewList: List<PostAPIModel> = homeViewModel.fetchBlogs(true)
-
             recyclerView.adapter = PostsViewAdapter(postViewList)
         }
 
