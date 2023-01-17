045a24143c25effd46cdca8dc3f09414fcb0b6ac
Melvin <melvintjiok@gmail.com>
2023-01-17 21:17:58 +0100

    add pagination to the user page
    For now, there is a bug that you need to refetch the profile page, e.g. go to another fragment and then again profile view. After that, the view is loaded and displayed.

--- app/src/main/java/com/example/blog_e/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/com/example/blog_e/ui/profile/ProfileViewModel.kt
@@ -5,6 +5,11 @@ import android.view.View
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.Pager
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import androidx.paging.liveData
 import com.example.blog_e.data.model.GetPostsQueryModel
 import com.example.blog_e.data.model.PostAPIModel
 import com.example.blog_e.data.model.ProfilePicture
@@ -22,6 +27,21 @@ class ProfileViewModel @Inject constructor(
 ) : ViewModel() {
 
     private val user: MutableLiveData<User?> = MutableLiveData()
+
+    private var _posts: MutableLiveData<PagingData<PostAPIModel>> =
+        Pager(
+            blogRepo.getDefaultPageConfig(),
+        ) {
+            PostPagingSource(
+                blogRepo,
+                isUserFeed = false,
+                usernames = listOf(DEFAULT_USER)
+            )
+        }.liveData
+            .cachedIn(viewModelScope) as MutableLiveData<PagingData<PostAPIModel>>
+
+
+    var posts: LiveData<PagingData<PostAPIModel>> = _posts
 
     fun getProfile(): LiveData<User?> {
         loadUserData()
@@ -74,6 +94,10 @@ class ProfileViewModel @Inject constructor(
             }
             is ApiException -> throw postsResult.e
         }
+    }
+
+    companion object {
+        const val DEFAULT_USER = "me"
     }
 
 }
