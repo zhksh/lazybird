303f991fd6b85201eda992c04fc874dbb943132a
Melvin <melvintjiok@gmail.com>
2023-01-18 23:32:14 +0100

    basic functionallity of fetching the user from the query
    Probably will only operate on ApiSuccess and on ApiError for the provided username.

--- app/src/main/java/com/example/blog_e/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/blog_e/ui/search/SearchFragment.kt
@@ -1,95 +1,76 @@
 package com.example.blog_e.ui.search
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.ImageButton
-import android.widget.ListView
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.SearchView.OnQueryTextListener
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.example.blog_e.Config
 import com.example.blog_e.databinding.FragmentSearchBinding
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
-
+@AndroidEntryPoint
 class SearchFragment : Fragment() {
 
     private var _binding: FragmentSearchBinding? = null
+    private val searchViewModel: SearchViewModel by viewModels()
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
 
-    //create variables.
-    lateinit var historyList: ArrayList<String>
-    lateinit var trendList: ArrayList<String>
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+
+        val TAG = Config.tag(this.toString())
         //init the view
-        val searchViewModel =
-            ViewModelProvider(this).get(SearchViewModel::class.java)
         _binding = FragmentSearchBinding.inflate(inflater, container, false)
         val root: View = binding.root
-        val textView: TextView = binding.textSearch
-        searchViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        //Bind some views about search history
-        val itemEdt = binding.editView
-        val listView: ListView = binding.historyList
-        val imageButton: ImageButton = binding.imageButton
-        historyList = ArrayList()
 
-        //Bind some views about trends
-        val listView2: ListView = binding.trendList
-        trendList = ArrayList()
-        trendList.add("Elon Musk")
-        trendList.add("Tesla")
-
-        //initialize adapter for our list view.
-        val adapter: ArrayAdapter<String?> = ArrayAdapter<String?>(
-            requireContext(),
-            android.R.layout.simple_list_item_1,
-            historyList as List<String?>
-        )
-        val adapter2: ArrayAdapter<String?> = ArrayAdapter<String?>(
-            requireContext(),
-            android.R.layout.simple_list_item_1,
-            trendList as List<String?>
-        )
-
-
-        //set adapter for our list view
-        listView.adapter = adapter
-        listView2.adapter = adapter2
-
-        //add click listener for our button.
-        imageButton.setOnClickListener {
-
-            //intent to results
-            Toast.makeText(this.context, "Search Results Page not Ready!", Toast.LENGTH_SHORT).show()
-            //get text from edit text
-            val item = itemEdt.text.toString()
-            //check if item is not empty
-            if (item.isNotEmpty()) {
-                //add item to our list.
-                historyList.add(item)
-                //notify adapter
-                //that data in list is updated to update our list view.
-                adapter.notifyDataSetChanged()
+        viewLifecycleOwner.lifecycleScope.launch {
+            searchViewModel.searchUIState.collect {
+                binding.searchView.setQuery(it.query, it.isSearching)
             }
         }
 
-        //item click events
-        //TODO
+        viewLifecycleOwner.lifecycleScope.launch {
+            searchViewModel.resultUIState.collect {
+            }
+        }
+
+
+        binding.searchView.setOnQueryTextListener(
+            object : OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    Log.i(TAG, "onQueryTextSubmit: $query")
+                    query?.let { performSearch(it) }
+                    return false
+                }
+
+                override fun onQueryTextChange(newQuery: String?): Boolean {
+                    searchViewModel.updateQuery(newQuery.toString())
+                    return false
+                }
+            }
+        )
 
         return root
+    }
+
+    private fun performSearch(query: String) {
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            searchViewModel.searchUser()
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/blog_e/ui/search/SearchViewModel.kt
+++ app/src/main/java/com/example/blog_e/ui/search/SearchViewModel.kt
@@ -1,13 +1,92 @@
 package com.example.blog_e.ui.search
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.blog_e.Config
+import com.example.blog_e.data.model.GetUserAPIModel
+import com.example.blog_e.data.repository.ApiError
+import com.example.blog_e.data.repository.ApiException
+import com.example.blog_e.data.repository.ApiSuccess
+import com.example.blog_e.data.repository.UserRepo
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import javax.inject.Inject
 
-class SearchViewModel : ViewModel() {
+data class SearchUIState(
+    val query: String = "",
+    val isSearching: Boolean = false
+)
+
+data class ResultUIState(
+    val isSuccessful: Boolean = false,
+    val userAPIModel: GetUserAPIModel? = null
+)
+
+@HiltViewModel
+class SearchViewModel @Inject constructor(
+    private val userRepo: UserRepo
+) : ViewModel() {
+    val TAG = Config.tag(this.toString())
 
     private val _text = MutableLiveData<String>().apply {
         value = "Recent searches"
     }
     val text: LiveData<String> = _text
+
+    private val _searchUIState = MutableStateFlow(SearchUIState())
+    val searchUIState: StateFlow<SearchUIState> = _searchUIState.asStateFlow()
+
+
+    private val _resultUIState = MutableStateFlow(ResultUIState())
+    val resultUIState: StateFlow<ResultUIState> = _resultUIState.asStateFlow()
+
+    suspend fun searchUser() {
+//        clearLastResult()
+        Log.i(TAG, "searchUser: ${searchUIState.value.query}")
+
+        val res = userRepo.getUser(_searchUIState.value.query)
+        when (res) {
+            is ApiSuccess -> {
+                updateCurrentUserResult(res.data)
+                Log.i(TAG, "searchUser: war erflogreich")
+            }
+            is ApiError -> {
+
+                Log.i(TAG, "searchUser: war fehler")
+            }
+            is ApiException -> Log.i(TAG, "searchUser: hat einen Fehler geworfen")
+        }
+    }
+
+    private fun clearLastResult() {
+        if (_resultUIState.value.isSuccessful) {
+            _resultUIState.update {
+                it.copy(
+                    userAPIModel = null,
+                    isSuccessful = false
+                )
+            }
+        }
+    }
+
+    private fun updateCurrentUserResult(data: GetUserAPIModel) {
+        _resultUIState.update {
+            it.copy(
+                userAPIModel = data,
+                isSuccessful = true
+            )
+        }
+    }
+
+    fun updateQuery(query: String) {
+        _searchUIState.update {
+            it.copy(query = query)
+        }
+    }
+
 }
